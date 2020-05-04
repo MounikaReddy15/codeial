fe893b045193db1f11a4f0e52b2cddeb394f3aad
unknown <mounikareddygonae15@gmail.com>
2020-05-04 23:15:17 +0530

    check at form and url action

--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,11 @@ const express = require('express');
 //express.Router helps in seperating routers and controllers
 const router = express.Router();
 
+//passport to authenticate
+const passport = require('passport');
+
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create',postsController.create);
+router.post('/create', passport.checkAuthentication,  postsController.create);
 
 module.exports = router;
