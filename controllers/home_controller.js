f6fbf5c1f622532fea34b497e6d2c806db154f4a
unknown <mounikareddygonae15@gmail.com>
2020-05-07 22:22:40 +0530

    show comment and the id

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -19,7 +19,17 @@ module.exports.home =function(req,res) {
     
    //callback func in exec
    //finding all posts and populate the user of each post
-   Post.find({}).populate('user').exec(function(err, posts){
+   Post.find({})
+   .populate('user')
+      // nested population
+   .populate({
+     path: 'comments',
+     populate: {
+      path: 'user'
+     }
+
+   })
+    .exec(function(err, posts){
        return res.render('home', {
     title: "Codeial | Home",
     posts: posts
