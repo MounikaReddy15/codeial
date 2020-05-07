55555dd05b670409df07691df6537fd57cee26da
unknown <mounikareddygonae15@gmail.com>
2020-05-07 23:18:20 +0530

    delete posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req,res) {
     Post.create({
@@ -9,5 +10,25 @@ module.exports.create = function(req,res) {
         if(err) { console.log('error in creating a post'); return;}
 
         return res.redirect('back');
-    })
+    });
+}
+
+module.exports.destroy = function(req,res) {
+    // find post if it exits or not to delete
+    Post.findById(req.params.id, function(err,post) {
+   // .id means converting the object id into String
+   //   if your comparing two obj id they both need to be string format
+        if(post.user == req.user.id){
+            post.remove();
+
+            // also delete comments
+            Comment.deleteMany({post: req.params.id}, function(err){
+                // func wont return anything becoz comments are deleted
+                return res.redirect('back');
+            });
+            //  if the user didn't match
+        }    else {
+            return res.redirect('back');
+        }
+    });
 }
